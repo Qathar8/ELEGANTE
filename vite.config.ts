4dc06e39d06315e048c9671730d0ab21e32cd234
Qathar8 <qureshqathar10@gmail.com>
2025-07-14 11:02:47 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // ✅ this is crucial for routing on Cloudflare Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
